24e5e6248493f46a72c1e57230b037d4f3643833
DinnoCreator <edijay17@gmail.com>
2023-02-17 16:38:41 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres1",
+    password: process.env.DB_PASSWORD,
+    host: "dpg-ce40bkha6gdkr7rutou0-a",
+    port: 5432,
+    database: "swizz"
+});
+
+module.exports = pool;
---
+++ utilities/acc.js
@@ -0,0 +1,37 @@
+exports.index = function() {
+    const iAccNo = Math.floor(Math.random()*10);
+    const iiAccNo = Math.floor(Math.random()*10);
+    const iiiAccNo = Math.floor(Math.random()*10);
+    const ivAccNo = Math.floor(Math.random()*10);
+    const vAccNo = Math.floor(Math.random()*10);
+    const viAccNo = Math.floor(Math.random()*10);
+    const viiAccNo = Math.floor(Math.random()*10);
+  
+    const theIndex = `${iAccNo}${iiAccNo}${iiiAccNo}${ivAccNo}${vAccNo}${viAccNo}${viiAccNo}`;
+  
+  
+    return theIndex;
+  };
+
+exports.index14 = function() {
+    const iAccNo = Math.floor(Math.random()*10);
+    const iiAccNo = Math.floor(Math.random()*10);
+    const iiiAccNo = Math.floor(Math.random()*10);
+    const ivAccNo = Math.floor(Math.random()*10);
+    const vAccNo = Math.floor(Math.random()*10);
+    const viAccNo = Math.floor(Math.random()*10);
+    const viiAccNo = Math.floor(Math.random()*10);
+    const viiiAccNo = Math.floor(Math.random()*10);
+    const ixAccNo = Math.floor(Math.random()*10);
+    const xAccNo = Math.floor(Math.random()*10);
+    const xiAccNo = Math.floor(Math.random()*10);
+    const xiiAccNo = Math.floor(Math.random()*10);
+    const xiiiAccNo = Math.floor(Math.random()*10);
+    const xivAccNo = Math.floor(Math.random()*10);
+  
+    const theIndex = `${iAccNo}${iiAccNo}${iiiAccNo}${ivAccNo}${vAccNo}${viAccNo}${viiAccNo}
+    ${viiiAccNo}${ixAccNo}${xAccNo}${xiAccNo}${xiiAccNo}${xiiiAccNo}${xivAccNo}`;
+  
+  
+    return theIndex;
+  };
