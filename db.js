56b5103b5bd173c232d6e61061730c3a68938a11
DinnoCreator <edijay17@gmail.com>
2023-02-18 06:56:46 +0100

    modified:   db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres1",
     password: process.env.DB_PASSWORD,
-    host: "dpg-ce40bkha6gdkr7rutou0-a.oregon-postgres.render.com",
+    host: "dpg-ce40bkha6gdkr7rutou0-a",
     port: 5432,
     database: "the_vault",
     ssl: true
