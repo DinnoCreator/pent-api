87321861df7bfe5b9e25478004b7f9b955879443
DinnoCreator <97753291+DinnoCreator@users.noreply.github.com>
2023-02-18 11:49:25 +0100

    Update db.js

--- db.js
+++ db.js
@@ -6,13 +6,7 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: "dpg-ce40bkha6gdkr7rutou0-a",
     port: 5432,
-    database: "the_vault",
-    ssl: {
-        rejectUnauthorized: false,
-        // ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
-        // key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
-        // cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
-      },
+    database: "the_vault"
 });
 
 module.exports = pool;
