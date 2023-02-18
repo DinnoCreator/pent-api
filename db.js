a59d6310cad7bbe261596d9b0fc78f72ace2c09f
DinnoCreator <edijay17@gmail.com>
2023-02-18 11:40:27 +0100

    modified:   db.js

--- db.js
+++ db.js
@@ -4,10 +4,15 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres1",
     password: process.env.DB_PASSWORD,
-    host: "dpg-ce40bkha6gdkr7rutou0-a.oregon-postgres.render.com",
+    host: "dpg-ce40bkha6gdkr7rutou0-a",
     port: 5432,
     database: "the_vault",
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false,
+        ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+        key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+        cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+      },
 });
 
 module.exports = pool;
